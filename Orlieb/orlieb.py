84b21108220ddf458759a8a14f10de2482c0ba6a
unknown <629@390-desktop.kth.kierantimberlake.com>
2017-08-01 17:43:24 -0400

    time series

--- Orlieb/orlieb.py
+++ Orlieb/orlieb.py
@@ -1,32 +1,53 @@
-import numpy as np 
 import pandas as pd
-import webbrowser
-import os
+from pandas import Series
+from sklearn.model_selection import train_test_split
+from sklearn import ensemble
+from sklearn.metrics import mean_absolute_error
+from sklearn.externals import joblib
+from matplotlib import pyplot
 
-import matplotlib.pyplot as plt
-from pylab import rcParams
-import seaborn as sb
+# Load the data set
+df = pd.read_csv("sample.csv")
 
-rcParams['figure.figsize'] = 5, 4
-sb.set_style('whitegrid')
+#Import dataset
+series = Series.from_csv('car-sale.csv', header=0)
+print(series.head(5))
 
-address = 'C:/Users/629/Documents/test_scripts/python/ML/Orlieb/2017_Jan-July_Poles (A_C_E)_15min.csv'
-df = pd.read_csv(address, index_col='Unix', date_parser=None )
+#line plot of data
+series.plot()
+pyplot.show()
 
-"""from pandas import Series, DataFrame
+# Replace categorical data with one-hot encoded data
+#features_df = pd.get_dummies(df, columns=['garage_type', 'city'])
+features_df = df
 
-missing = np.nan
+# Create the X and y arrays
+X = features_df.as_matrix()
+y = df['sale_price'].as_matrix()
 
-df = pd.read_csv("2017_Jan-July_Poles (A_C_E)_15min.csv")
-df = df.transpose()"""
+# Split the data set in a training set (70%) and a test set (30%)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
 
-html = df[0:6].to_html()
-# Save the html to a temporary file
-with open("data.html", "w") as f:
-    f.write(html)
+# Fit regression model
+model = ensemble.GradientBoostingRegressor(
+    n_estimators=1000,
+    learning_rate=0.1,
+    max_depth=6,
+    min_samples_leaf=9,
+    max_features=0.1,
+    loss='huber',
+    random_state=0
+)
+model.fit(X_train, y_train)
 
-# Open the web page in our web browser
-full_filename = os.path.abspath("data.html")
-webbrowser.open("file://{}".format(full_filename))
+# Save the trained model to a file so we can use it in other programs
+joblib.dump(model, 'trained_house_classifier_model.pkl')
 
+# Find the error rate on the training set
+mse = mean_absolute_error(y_train, model.predict(X_train))
+print("Training Set Mean Absolute Error: %.4f" % mse)
+
+# Find the error rate on the test set
+mse = mean_absolute_error(y_test, model.predict(X_test))
+print("Test Set Mean Absolute Error: %.4f" % mse)
 
